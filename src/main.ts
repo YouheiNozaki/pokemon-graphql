26d10a064ce5cd819d9f256eea494e3d358f1c8c
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-01-30 23:23:37 +0900

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
-import { createApp } from 'vue';
+import { createApp, provide, h } from 'vue';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { DefaultApolloClient } from '@vue/apollo-composable';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const defaultClient = new ApolloClient({
+  uri: 'https://graphql-pokemon2.vercel.app',
+  cache: new InMemoryCache(),
+});
+
+createApp(
+  {
+    setup() {
+      provide(DefaultApolloClient, defaultClient);
+    },
+    render() {
+      return h(App);
+    },
+  },
+).mount('#app');
